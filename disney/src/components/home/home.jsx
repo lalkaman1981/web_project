14311475fea9cd872af3e9c03b39d9501975f4b5
Fenix125 <migelspainn@gmail.com>
2025-04-14 16:52:54 +0300

    added api fetch

--- disney/src/components/home/home.jsx
+++ disney/src/components/home/home.jsx
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from 'react';
+
 import disneyLogo from "../../assets/images/home/disney.png";
 import disneyLogo2 from "../../assets/images/home/logoDisney.png";
 import StarWarsLogo from "../../assets/images/home/star_wars.png";
@@ -6,7 +8,6 @@ import NatureLogo from "../../assets/images/home/nature.png";
 import PixarLogo from "../../assets/images/home/pixar.png";
 import avatarLogo from "../../assets/images/home/Avatar.png";
 
-import bg from "../../assets/images/home/cover.png";
 
 import homeLogo from "../../assets/images/home/home.png";
 import SeriesLogo from "../../assets/images/home/tv.png";
@@ -19,21 +20,80 @@ import PlayLogo from "../../assets/images/home/play.png";
 import InfoLogo from "../../assets/images/home/info.png";
 
 
-import FilmLogo from "../../assets/images/home/Movielogo.png";
-
 import Overlay1 from "../../assets/images/home/overlay1.png";
 import Overlay2 from "../../assets/images/home/overlay1.png";
+import OverlayTop from "../../assets/images/home/overlaytop.png";
 
 import styles from "../../assets/styles/home/home.module.css";
 
 
+const API_URL = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
+
+
 function Home(){
-    const filmDescr = "La película de Disney y Pixar “Luca” está ambientada en un pueblo de la costa italiana y cuenta la historia de un adolescente que pasa un verano inolvidable lleno de aventuras junto con su nuevo amigo Alberto.";
+    const [movie, setMovie] = useState(null);
+    const [logoUrl, setLogoUrl] = useState(null);
+
+    const fetchLogo = async (movieId) => {
+        try {
+            const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/images`, {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: `Bearer ${API_KEY}`,
+                },
+            });
+            const data = await response.json();
+            const enLogos = data.logos?.filter(logo => logo.iso_639_1 === 'en');
+            if (enLogos && enLogos.length > 0) {
+                setLogoUrl(`${IMAGE_BASE_URL}${enLogos[0].file_path}`);
+            }
+        } catch (error) {
+            console.error('Error fetching logo:', error);
+        }
+    };
+    const fetchMovie = async () => {
+        try {
+            const response = await fetch(API_URL, {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: `Bearer ${API_KEY}`,
+                },
+            });
+            const data = await response.json();
+            const mostPopularMovie = data.results[0];
+            setMovie(mostPopularMovie);
+            fetchLogo(mostPopularMovie.id)
+        } catch (error) {
+            console.error('Error fetching movie data:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchMovie();
+        
+        const interval = setInterval(fetchMovie, 86400000);
+    
+        return () => clearInterval(interval);
+    }, []);
+
+
+    if (!movie) return <div>Loading...</div>;
+    if (!logoUrl) return <div>Loading...</div>;
+    
+    console.log(movie)
+
+    const bgImage = `${IMAGE_BASE_URL}${movie.backdrop_path}`;
+    const movieDecrp = `${movie.overview}`;
+    
     return(
         <div className={styles.HomeContainer}>
-            <img className={styles.bg_image} src={bg}></img>
+            <img className={styles.bg_image} src={bgImage}></img>
             <img className={styles.Overlay1} src={Overlay1}></img>
             <img className={styles.Overlay2} src={Overlay2}></img>
+            <img className={styles.OverlayTop} src={OverlayTop}></img>
             <header className={styles.header}>
                 <nav className={styles.header_nav_left}>
                     <img className={styles.disney_logo} src={disneyLogo2}></img>
@@ -69,9 +129,9 @@ function Home(){
 
             <section className={styles.film}>
                 <div className={styles.film_logo_text}>
-                    <img src={FilmLogo} alt="" />
+                    <img className={styles.logo_img} src={logoUrl} alt="" />
                     <p className={styles.film_text}>
-                        {filmDescr}
+                        {movieDecrp}
                     </p>
                 </div>
                 <div className={styles.film_bttns}>
